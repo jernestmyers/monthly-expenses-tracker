1eb363e677bbbc87f0f6da5fba6e8a02d3c1cc09
Jeremy Myers <inernestmusic@gmail.com>
2024-06-05 10:58:48 -0400

    Configure db root cert

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,10 +1,19 @@
 import { Pool } from 'pg';
 import dotenv from 'dotenv';
+import fs from 'fs';
 
 dotenv.config({ path: '../.env' });
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ...(process.env.NODE_ENV === 'production' && process.env.PGSSLROOTCERT
+    ? {
+        ssl: {
+          rejectUnauthorized: true,
+          ca: fs.readFileSync(process.env.PGSSLROOTCERT).toString(),
+        },
+      }
+    : {}),
 });
 
 export default pool;

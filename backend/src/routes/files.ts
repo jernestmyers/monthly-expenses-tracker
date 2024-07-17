e22e2a965f6119cb229a0e7f6e2744a2cc82bb4b
Jeremy Myers <inernestmusic@gmail.com>
2024-07-17 14:48:44 -0400

    Create tables to persist transaction details

--- backend/src/routes/files.ts
+++ backend/src/routes/files.ts
@@ -74,9 +74,48 @@ router.post(
   },
 );
 
-router.post('/submit', authenticateJWT, (req, res) => {
-  console.log(req.body);
-  res.sendStatus(201);
+router.post('/submit', authenticateJWT, async (req, res) => {
+  const tabUrlParts = req.headers['referer']?.split('/');
+  const transactions = req.body;
+  if (!Array.isArray(transactions) || !transactions.length || !tabUrlParts) {
+    return res.status(400).send('Invalid request');
+  }
+
+  try {
+    const user = req.user as { id: number; username: string };
+    const { id } = user;
+    const client = await pool.connect();
+    transactions.forEach(async (t) => {
+      const newTransaction = await client.query(
+        `
+        INSERT INTO transactions (user_id, payer_id, date, amount, description, memo)
+        VALUES ($1, $2, $3, $4, $5, $6)
+        RETURNING id
+      `,
+        [id, Number(t.paidBy), t.date, t.amount, t.description, t.memo],
+      );
+      await client.query(
+        `
+        INSERT INTO transaction_categories (transaction_id, category_id)
+        VALUES ($1, $2)
+      `,
+        [newTransaction.rows[0].id, Number(t.category)],
+      );
+    });
+
+    const currentTabDatePrefix = tabUrlParts[3] + '-' + tabUrlParts[4];
+    const currentTabTransactions = await client.query(`
+      SELECT t.*, c.name AS category_name, c2.name AS parent_category_name, p.name AS payer_name
+      FROM transactions t
+      LEFT JOIN transaction_categories tc ON t.id = tc.transaction_id
+      LEFT JOIN categories c ON tc.category_id = c.id
+      LEFT JOIN categories c2 ON c.parent_id = c2.id
+      LEFT JOIN payers p ON t.payer_id = p.id
+      WHERE t.user_id = ${id} AND t.date >= '${currentTabDatePrefix + '-1'}' AND t.date <= '${currentTabDatePrefix + '-31'}';    
+    `);
+    client.release();
+    res.status(200).json(currentTabTransactions.rows);
+  } catch (err) {}
 });
 
 export default router;

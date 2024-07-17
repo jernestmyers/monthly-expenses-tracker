0f55fd1773107e36d8c51c40131b248ad1cfd881
Jeremy Myers <inernestmusic@gmail.com>
2024-07-17 13:20:42 -0400

    Format CapitalOne date to align with Chase and Schwab's format

--- backend/src/utils/formatCapitalOneCreditCardCsv.ts
+++ backend/src/utils/formatCapitalOneCreditCardCsv.ts
@@ -7,9 +7,15 @@ export function formatCapitalOneCreditCardCsv(
     .filter((d) => d['Transaction Date'].length)
     .map((d) => ({
       id: d.id,
-      date: d['Transaction Date'],
+      date: formatDate(d['Transaction Date']),
       description: d['Description'],
       memo: '',
       amount: d['Debit'] ? Number(d['Debit']) * -1 : Number(d['Credit']),
     }));
+}
+
+function formatDate(date: string) {
+  // CapitalOne dates are YYYY-MM-DD and we want to convert to MM/DD/YYYY
+  const parts = date.split('-');
+  return parts[1] + '/' + parts[2] + '/' + parts[0];
 }

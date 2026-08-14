9ea88015897f45ce103093a3e93626c0fbfbd5dd
Jeremy Myers <inernestmusic@gmail.com>
2026-08-14 15:54:03 -0400

    Upgrade react and convert from webpack to vite

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import globals from 'globals';
 import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js';
+import pluginReactJsxRuntime from 'eslint-plugin-react/configs/jsx-runtime.js';
 import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default [
@@ -9,5 +10,7 @@ export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReactConfig,
+  pluginReactJsxRuntime,
+  { settings: { react: { version: 'detect' } } },
   eslintConfigPrettier,
 ];
